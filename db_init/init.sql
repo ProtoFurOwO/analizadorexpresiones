bc9239960be1e8f96904bf23bf527aea46d66b1d
ProtoFurOwO <j0s3kni3@gmail.com>
2026-03-15 00:01:37 -0600

    actualizacion de sql

--- db_init/init.sql
+++ db_init/init.sql
@@ -2,5 +2,6 @@ CREATE TABLE historial_consultas (
     id SERIAL PRIMARY KEY,
     query_text TEXT NOT NULL,
     is_valid BOOLEAN NOT NULL,
+    matches_count INTEGER DEFAULT 0,
     fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
